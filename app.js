0644c9bf90ea4a0a9df2decf7721cb635d0d93a3
pshenopro <n.psheno@gmail.com>
2020-05-14 13:13:42 +0300

    bug fix

--- app.js
+++ app.js
@@ -10,42 +10,50 @@ let pathHttp = '';
 
 app.use(express.json({extended: true}));
 
+if (process.env.NODE_ENV === 'prod') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     pathHttp = req.path;
     next();
 });
 
-// app.use(pathHttp, (req, res, next) => {
-//     if (pathHttp === '/') {
-//         return;
-//     }
-//
-//     const data = { ...req.body };
-//
-//     console.log('after / ' + pathHttp);
-//
-//     request({
-//         url: backAPI + posts[pathHttp].url,
-//         method: posts[pathHttp].method,
-//         json: true,   // <--Very important!!!
-//         body: data
-//     }, function (error, response, body) {
-//         if (error) {
-//             res.status(500);
-//             res.json({message: 'SERVER ERROR'});
-//             next();
-//
-//             return
-//         }
-//
-//         if (response.body.status === 'error') {
-//             res.status(400)
-//         }
-//
-//         res.json(response.body)
-//     });
-// });
+app.use(pathHttp, (req, res, next) => {
+    if (pathHttp === '/') {
+        return;
+    }
+
+    const data = { ...req.body };
+
+    console.log('after / ' + pathHttp);
+
+    request({
+        url: backAPI + posts[pathHttp].url,
+        method: posts[pathHttp].method,
+        json: true,   // <--Very important!!!
+        body: data
+    }, function (error, response, body) {
+        if (error) {
+            res.status(500);
+            res.json({message: 'SERVER ERROR'});
+            next();
+
+            return
+        }
+
+        if (response.body.status === 'error') {
+            res.status(400)
+        }
+
+        res.json(response.body)
+    });
+});
 
 const PORT = config.get('port') || 5000;
 
